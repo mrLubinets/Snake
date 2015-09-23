d14a28942a6ae2ecb5382131916974002c951865
mrLubinets <mr.lubinets@gmail.com>
2015-09-23 13:31:54 +0300

    All norm 2 lessons

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,5 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DUCK KRA KRA");
+            int x1, y1,x2,y2;
+            x1 = 1;
+            y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
             Console.ReadLine();
+
+            
+        }
+
+
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
+        }
+    }
+}
