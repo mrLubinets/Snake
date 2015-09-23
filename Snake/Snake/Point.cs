1ab568d02a0448538fbb4dcda6edd60264f97d2a
mrLubinets <mr.lubinets@gmail.com>
2015-09-23 13:48:17 +0300

    Good go my brain C#

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+       public  int x, y;
+       public  char sym;
+
+       public void draw()
+        {
+            Console.SetCursorPosition( x, y );
+            Console.Write( sym );
+
+        }
+    }
+    
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,24 +10,33 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1, y1,x2,y2;
+            /*int x1, y1,x2,y2;
             x1 = 1;
             y1 = 3;
             char sym1 = '*';
 
             Console.SetCursorPosition(x1, y1);
             Console.Write(sym1);
+            */
+
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 6;
+            p1.sym = '#';
+            p1.draw();
+
+            Point p2 = new Point();
+            p2.x = 6;
+            p2.y = 6;
+            p2.sym = '!';
+            p2.draw();
+
             Console.ReadLine();
 
             
         }
 
 
-        static void draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
-        }
+      
     }
 }
