776cc03297e277d9472f74e0ab827540682ca2f6
mrLubinets <mr.lubinets@gmail.com>
2015-09-24 23:44:08 +0300

    Баловался со списками)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -19,17 +19,55 @@ namespace Snake
             Console.Write(sym1);
             */
 
-            Point p1 = new Point(1,10,'!');
+            Point_k p1 = new Point_k(1,10,'!');
           /*  p1.x = 1;
             p1.y = 6;
             p1.sym = '#';*/
             p1.draw();
 
-            Point p2 = new Point(10,16,'f');
+            Point_k p2 = new Point_k(10,16,'f');
             /*p2.x = 6;
             p2.y = 6;
             p2.sym = '!';*/
             p2.draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(7); // иницилизация первого элемента в списке
+            numList.Add(1);
+            numList.Add(1);
+
+
+            int x = numList[0]; // обрвщиение к первому элементу списка
+            int y = numList[1];
+            int z = numList[2];
+
+            //Вывод элементов через foreach()
+
+         /*  foreach (int i in numList) {
+               Console.WriteLine(i);
+            }
+            */
+
+            //Удаление из списка 
+            Console.WriteLine("Удаляем элемент из списка");
+            Console.WriteLine(numList[0]);
+            numList.Remove(1);
+            Console.WriteLine(numList[0]);
+
+
+            /*  foreach (int i in numList)
+              {
+                  Console.WriteLine(i);
+              }
+
+
+  */
+
+            List <Point_k> pList = new List <Point_k>();
+            pList.Add(p2);
+
+            LIST l = new LIST();
+            l.create_09();
 
             Console.ReadLine();
 
